86d94dec89115abc378a9505bf8499e7052838b4
issei_ezawa <issemann@yahoo.co.jp>
2021-09-14 00:28:33 +0900

    genba_rails Capter4-4~5

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,7 +2,7 @@ class Task < ApplicationRecord
     validates :name, presence: true
     validates :name, length: { maximum: 30 }
     validate :validate_name_not_including_comma
-    before_valitation :set_nameless_name
+    before_validation :set_nameless_name
 
     private
 
