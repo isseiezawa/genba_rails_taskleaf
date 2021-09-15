dc65f28f52509b93ead9afa3c3879948d8a61737
issei_ezawa <issemann@yahoo.co.jp>
2021-09-15 10:57:28 +0900

    genba_rails Capter4-6~10

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,28 @@
+class SessionsController < ApplicationController
+  skip_before_action :login_required
+
+  def new
+  end
+
+  def create
+    user = User.find_by(email: sessions_params[:email])
+
+    if user&.authenticate(sessions_params[:email])
+      session[:user_id] = user.id
+      redirect_to root_url, notice: 'ログインしました。'
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    reset_session
+    redirect_to root_url, notice: 'ログアウトしました。'
+  end
+
+  private
+
+  def sessions_params
+    params.require(:session).permit(:email, :password)
+  end
+end
